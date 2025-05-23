26155bb1c1c11486bbc6ca54ea6518d4ef356b28
tushar <tushar789imran@gmail.com>
2025-05-23 10:59:45 +0600

    project setup and navbar and banner part done

---
+++ app/_components/banner.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ArrowDownRight } from 'lucide-react';
+import BottomArrowIcon from '@/public/icons/bottom-arrow-icon';
+
+export default function Banner() {
+    return (
+        <div className='container py-28'>
+            <div className="relative text-white flex flex-col justify-center items-start px-8 sm:px-20">
+                {/* Small Intro Text */}
+                <p className="text-base sm:text-xl mb-4">Hello! I'm Maguire</p>
+
+                {/* Headline */}
+                <h1 className="text-[2.5rem] sm:text-[100px] leading-[120%] font-bold">
+                    <div>Designing</div>
+                    <div>
+                        <span className="bg-gradient-to-r from-[#FDC14C] via-[#EA5CFB] to-[#2E5CFF] text-transparent bg-clip-text">
+                            Digital Product
+                        </span>
+                    </div>
+                    <div>With Emphasis</div>
+                    <div>On Visual</div>
+                    <div>Design</div>
+                </h1>
+
+                {/* Arrow Icon */}
+                <div className="mt-10 absolute -bottom-20 left-[450px]">
+                    <BottomArrowIcon />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ app/_components/navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between py-8">
+                {/* Logo */}
+                <div>
+                    <h1 className="text-[40px] font-bold">Mg<span className="text-blue-500">.</span></h1>
+                </div>
+
+                {/* Gradient Shadow Button */}
+                <div className="relative inline-block">
+                    {/* Skewed Gradient Background */}
+                    <div className="absolute inset-0 z-0 transform -rotate-[5deg] rounded-[20px]">
+                        <div className="w-full h-full bg-gradient-to-r from-[#FDC14C] via-[#EA5CFB] to-[#2E5CFF] rounded-md" />
+                    </div>
+
+                    {/* Top White Button */}
+                    <button className="relative z-10 bg-white text-[#151414] text-lg md:text-xl font-medium px-8 py-3 rounded-md transition hover:bg-gray-100">
+                        Let’s Talk
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
